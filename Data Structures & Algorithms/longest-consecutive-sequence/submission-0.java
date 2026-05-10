79d9a880d355d198f36225e7385576eece607695
Vinod <O-OF-N@users.noreply.github.com>
2026-05-10 18:07:21 -0400

    Add: longest-consecutive-sequence - submission-0

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-0.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        int n = nums.length;
+        Map<Integer, Integer> minMax = new HashMap<>();
+        Set<Integer> allElements = new HashSet<>();
+        Set<Integer> evaluated = new HashSet<>();
+        for(int i=0;i<n;i++) {
+            int num = nums[i];
+            allElements.add(num);
+        }
+        for(int i=0; i < n;i++) {
+            int num = nums[i];
+            if(evaluated.contains(num)) {
+                continue;
+            } 
+            int nextNum = num+1;
+            if(minMax.containsKey(nextNum)) {
+                int max= minMax.get(nextNum); 
+                minMax.remove(nextNum);
+                minMax.put(num , max);
+                continue;
+            }
+            int max = num;
+            while(allElements.contains(max)) {
+                evaluated.add(max);
+                max = max+1;
+            }
+            minMax.put(num, max-1);
+        }
+        System.out.println(minMax);
+        int maxLength = 0;
+        Set<Integer> allMinValues = minMax.keySet();
+        System.out.println(allMinValues);
+        for(int minValue:allMinValues) {
+            int maxValue = minMax.get(minValue);
+            int length = maxValue - minValue + 1;
+            maxLength = Math.max(maxLength, length);
+        }
+
+        return maxLength;
+    }
+}
